2de37fdc96f1e7856912cb57bca372806b6b0729
Despina22 <stankovskadespina22@gmail.com>
2023-02-16 21:29:20 +0100

    feat: Candidate class

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Candidate } from './models/candidate.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'My First app';
+  candidate1 = new Candidate('William', 'Jones', '15-04-1988');
+  candidate2 = new Candidate('Olivia', 'Perry', '22-07-1996');
+
+  ngOnInit(): void {
+    console.log(this.candidate1, this.candidate2);
+  }
 }
---
+++ HelloWorld/src/app/models/candidate.model.ts
@@ -0,0 +1,11 @@
+export class Candidate {
+  firstName: string;
+  lastName: string;
+  birthday: string;
+
+  constructor(firstName: string, lastName: string, birthday: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = birthday;
+  }
+}
