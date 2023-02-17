6c0b1adf81ed8508e0a79b1b95416e68252e0d91
Despina22 <stankovskadespina22@gmail.com>
2023-02-17 12:48:50 +0100

    feat: Employee class

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Employee } from './models/employee.model';
 import { Component, OnInit } from '@angular/core';
 import { Candidate } from './models/candidate.model';
 
@@ -11,6 +12,21 @@ export class AppComponent implements OnInit {
   candidate1 = new Candidate('William', 'Jones', new Date(1988, 3, 15));
   candidate2 = new Candidate('Olivia', 'Perry', new Date(1996, 6, 22));
 
+  employee1 = new Employee(
+    1,
+    'David',
+    'david123@gmail.com',
+    123 - 654 - 789,
+    'Architect'
+  );
+  employee2 = new Employee(
+    2,
+    'Max',
+    'max456@gmail.com',
+    321 - 654 - 789,
+    'Attorney'
+  );
+
   ngOnInit(): void {
     console.log(this.candidate1, this.candidate2);
 
@@ -19,6 +35,9 @@ export class AppComponent implements OnInit {
 
     const printCandidates = this.printCandidates(candidates);
     console.log(printCandidates);
+
+    console.log('Employee 1:', this.employee1);
+    console.log('Employee 2:', this.employee2);
   }
 
   printCandidates(candidates: Candidate[]): string {
