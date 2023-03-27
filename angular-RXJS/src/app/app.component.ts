512e64839b31f0a8afe612232da05f5a7e66c666
Despina22 <stankovskadespina22@gmail.com>
2023-03-27 18:42:36 +0200

    feat: Current Time - Inteval

--- angular-RXJS/src/app/app.component.ts
+++ angular-RXJS/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-RXJS';
+
+  currentTime$: Observable<Date> = new Observable<Date>((observer) => {
+    setInterval(() => {
+      observer.next(new Date());
+    }, 1000);
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.currentTime$.subscribe((time) => console.log(time));
+  }
 }
